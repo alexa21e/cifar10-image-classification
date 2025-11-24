d6b02e9415b49da35f66dede363864a1207dd2bb
alexa21e <pirvuletu.e.alexandra@gmail.com>
2025-11-24 22:55:15 +0200

    Define the baseline 2-layer CNN model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 def load_and_preprocess_cifar10():
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -18,9 +20,64 @@ def load_and_preprocess_cifar10():
 
     return (x_train, y_train_encoded), (x_test, y_test_encoded)
 
+def create_baseline_cnn(input_shape=(32, 32, 3), num_classes=10):
+    # Sequential([]) defines the model as a sequential stack of layers
+    # where output from one layer feeds directly into the next
+    model = Sequential([
+
+        # Conv2D() applies 32 filters (feature detectors) of size 3x3
+        # relu (Rectified Linear Unit) is the activation function
+
+        # This layer learns the edges and the corners from the input image
+        Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+
+        # MaxPooling2D() reduces the resolution of the feature maps by taking the maximum value in a 2x2 window
+
+        # This reduces the spatial size from 32x32 to 16x16 
+        # lowering computation and helping the model become more robust to variations in image position.
+        MaxPooling2D((2, 2)),
+
+        # A 2nd Conv2D layer learns more complex features like shapes and textures(abstract features) using 64 filters
+        # Increases the complexity of learned features by combining with the output of the first layer
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+
+        # Flatten() converts the 2D feature maps into a 1D feature vector
+
+        # It is necessary to transition from the convolutional layers to 
+        # the standard fully connected (Dense) layers for classification
+        Flatten(),
+
+        # Dense() is a fully connected layer with 100 neurons and relu activation
+
+        # Performs high-level reasoning on the extracted 1D features before classification
+        Dense(100, activation='relu'),
+
+        # The final Dense layer outputs a probability distribution over the 10 classes using softmax activation
+        # It outputs a dense layer with 10 neurons as the final classification 
+        # softmax ensures the output values are probabilities that sum up to 1
+        Dense(num_classes, activation='softmax')
+    ])
+
+    # model.compile(...) configures the learning process before training begins
+    # optimizer='adam' is the algorithm used to adjust the model's weights during backpropagation to minimize the loss
+    # loss='categorical_crossentropy' is the metric used to calculate the error between the prediction and the true label
+    # It's the standard choice for multi-class classification with one-hot encoded labels.
+    # metrics=['accuracy'] is the value displayed during and after training to easily gauge model performance.
+    
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+                  
+    return model
+
 if __name__ == '__main__':
     (features_train, labels_train), (features_test, labels_test) = load_and_preprocess_cifar10()
     assert features_train.shape == (50000, 32, 32, 3)
     assert labels_train.shape == (50000, 10)
     assert features_train.max() <= 1.0 and labels_train.min() >= 0.0
     print("Data shapes and normalization verified successfully!")
+
+    create_baseline_cnn()
+    model = create_baseline_cnn()
+    model.summary()
